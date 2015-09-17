1cc7e74dfa79e1c1ec0aebbfa6ee26cd0989d118
Brandon Morgado <morgadob@amazon.com>
2015-09-17 13:24:11 -0700

    -added dependencies in README.md
    -initial checkin of code

---
+++ LimitCheck.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+# programmatic polling and SNS notification of TA limit checks
+
+Limit () {
+echo "Contacting Trusted Advisor..."
+
+aws support describe-trusted-advisor-check-result --check-id "eW7HH0l7J9" --query 'result.sort_by(flaggedResources[?status!=`ok`],&metadata[2])' | jq  '.[].metadata' > limitvalue.txt
+# eW7HH0l7J9 - service limits
+# S45wrEXrLz - VPN Tunnel Redundancy
+}
+
+Publish () {
+echo "Publishing message to SNS topic..."
+
+aws sns publish --topic-arn arn:aws:sns:us-west-2:141820633316:AWS-Limits --message "You have limits approaching their upper threshold. Please take action accordingly."
+}
+
+Limit
+
+if [ -s limitvalue.txt ]
+then
+  echo "The value is: "
+  cat limitvalue.txt
+  Publish
+else
+  echo "All systems green!"
+fi
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # AWSLimitCheck
+
+Dependencies:
+1) Install the AWS CLI from http://aws.amazon.com/cli
+
+2) Install the opensource tool "jq"
+#sudo yum install jq.x86_64
+
